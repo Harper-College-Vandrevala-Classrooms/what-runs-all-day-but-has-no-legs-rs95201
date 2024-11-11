ba60dc89b65b21716b730ad838c5850851d377e0
rs95201 <114031245+rs95201@users.noreply.github.com>
2024-11-10 22:41:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <cassert>
 #include "time.hpp"
+
 int main()
 {
     Timer timer;
-   
-    //timer.start_time(); 
-    std::cout << "System time: " << std::endl;
+    std::cout << "System time when timer starts: " << std::endl;
+    timer.start_time();  
     timer.get_the_system_time();
+
     timer.wait_for_few_seconds();  
 
-    timer.stop_time(); 
+    timer.stop_time();  
 
-    std::cout << "System time: " << std::endl;
+    std::cout << "System time when timer stops: " << std::endl;
     timer.get_the_system_time();
 
     time_t elapsed_time = timer.elapsed_time();  
     std::cout << "Elapsed time: " << elapsed_time << " seconds." << std::endl;
-    assert(elapsed_time == 5);
-   std::cout << "All assertions passed" << std:: endl;
+
+    assert(elapsed_time == 3);
+    std::cout << "All assertions passed" << std::endl;
 
     return 0;
 }
-
-
