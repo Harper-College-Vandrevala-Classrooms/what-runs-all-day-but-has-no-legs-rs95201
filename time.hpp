41523d7ddae13a1ae26cb96e13373fb02c649b5d
rs95201 <114031245+rs95201@users.noreply.github.com>
2024-11-10 23:45:56 -0600

    Update time.hpp

--- time.hpp
+++ time.hpp
@@ -9,10 +9,11 @@ private:
 
 public:
     Timer();
+
     void start_time();  
     void stop_time();   
     time_t elapsed_time();
-    void get_the_system_time() const;
     void wait_for_few_seconds();
+    long get_the_system_time() const;
 };
 #endif
