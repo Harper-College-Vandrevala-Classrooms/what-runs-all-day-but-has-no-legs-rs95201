c6dfb050be829f91503cf61023795e463d669703
rs95201 <114031245+rs95201@users.noreply.github.com>
2024-11-10 23:45:23 -0600

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -23,7 +23,6 @@ void Timer::stop_time()
     else
     {
         std::cout << "Error: The timer has not been started!" << std::endl;
-        exit(EXIT_FAILURE);  
     }
 }
 
@@ -36,30 +35,11 @@ time_t Timer::elapsed_time()
     return elapsed;  
 }
 
-void Timer::get_the_system_time() const
-{
-    constexpr short sec_per_min = 60,
-                  min_per_hour = 60,
-                  sec_per_hour = sec_per_min * min_per_hour, 
-                  hrs_per_day = 24;
-    constexpr long sec_per_day = static_cast<long>(sec_per_hour) * hrs_per_day;
-
-    long sec_today = time(nullptr) % sec_per_day;
-
-    short hour = static_cast<short>(sec_today / sec_per_hour),
-          sec_not_hour = static_cast<short>(sec_today % sec_per_hour),
-          min = sec_not_hour / sec_per_min,
-          sec = sec_not_hour % sec_per_min;
-
-    std::cout << hour << ':';
-    std::cout.fill('0');
-    std::cout.width(2);
-    std::cout << min << ':';
-    std::cout.width(2);
-    std::cout << sec << ".\n";
-}
-
 void Timer::wait_for_few_seconds()
 {
     std::this_thread::sleep_for(std::chrono::seconds(3)); 
+}
+long Timer::get_the_system_time() const
+{
+    return time(nullptr); 
 }
