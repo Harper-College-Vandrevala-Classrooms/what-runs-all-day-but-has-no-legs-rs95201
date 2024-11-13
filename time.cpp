f305021258054803e9f23fbbcf6e89dc3a6e389d
rs95201 <114031245+rs95201@users.noreply.github.com>
2024-11-13 15:06:21 -0600

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -39,7 +39,16 @@ void Timer::wait_for_few_seconds()
 {
     std::this_thread::sleep_for(std::chrono::seconds(3)); 
 }
-long Timer::get_the_system_time() const
+long Timer::get_the_system_time() 
 {
-    return time(nullptr); 
+    constexpr short sec_per_min = 60;
+    constexpr short min_per_hour = 60;
+    constexpr short sec_per_hour = sec_per_min * min_per_hour; 
+    constexpr short hrs_per_day = 24;
+    constexpr long sec_per_day = static_cast<long>(sec_per_hour) * hrs_per_day;
+
+    long sec_today = time(nullptr) % sec_per_day;  // Get seconds for the current day
+
+    return sec_today;
 }
+
